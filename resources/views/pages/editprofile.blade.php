da275aa40bb568c9f8046c548640bc579b381201
DiogoRamos9 <up202207954@up.pt>
2024-12-19 16:28:49 +0000

    Fix erros

--- resources/views/pages/editprofile.blade.php
+++ resources/views/pages/editprofile.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container d-flex justify-content-center align-items-center" style="min-height: calc(100vh - 100px); margin-top: 50px; margin-bottom: 50px;">
     <div class="row w-100">
         <div class="col-md-6 mx-auto">
@@ -53,48 +54,15 @@
                         <textarea name="bio" id="bio" class="form-control" rows="3">{{ old('bio', $user->bio) }}</textarea>
                     </div>
 
+                    @include('pages.tags')
+        
+                    @include('partials.toast')
+
                     <button type="submit" class="btn btn-primary w-100">Save Changes</button>
                 </form>
             </div>
         </div>
-    @endif
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <form method="POST" action="{{ isset($isAdminEditing) && $isAdminEditing ? route('profile.updateAny', $user->username) : route('profile.update') }}">
-        @csrf
-        @method('PATCH')
-
-        <div class="form-group">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" name="name" id="name" class="form-control" value="{{ old('name', $user->name) }}" required>
-        </div>
-
-        <div class="form-group">
-            <label for="username" class="form-label">Username</label>
-            <input type="text" name="username" id="username" class="form-control" value="{{ old('username', $user->username) }}" required>
-        </div>
-
-        <div class="form-group">
-            <label for="bio" class="form-label">Bio</label>
-            <textarea name="bio" id="bio" class="form-control" rows="3">{{ old('bio', $user->bio) }}</textarea>
-        </div>
-
-        @include('pages.tags')
-        
-        @include('partials.toast')
-
-        <button type="submit" class="button">Save Changes</button>
-    </form>
-</div>
 
 <script>const max_tags = 5</script>
 <script>const tags = @json($tags);</script>
