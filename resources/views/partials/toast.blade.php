b945296cd65510bde9664f800ccf9c6607717019
daniel-gomes-silva <up201909935@fe.up.pt>
2024-12-19 16:50:03 +0000

    toast template view

--- resources/views/partials/toast.blade.php
+++ resources/views/partials/toast.blade.php
@@ -1,10 +1,8 @@
-@if(session('toast'))
-<div class="toast position-fixed bottom-0 end-0 p-3 bg-warning" role="alert" aria-live="assertive" aria-atomic="true" data-bs-autohide="true">
+<div class="toast position-fixed bottom-0 end-0 p-3 bg-warning" role="alert" aria-live="assertive" aria-atomic="true">
     <div class="d-flex">
         <div class="toast-body">
-            {{ session('toast') }}
         </div>
         <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>    
     </div>
 </div>
-@endif
+
